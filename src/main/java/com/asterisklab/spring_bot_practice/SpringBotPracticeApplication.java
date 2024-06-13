a6fcfc105f16fce39188723dc04b8023049743b1
Sabir Islam Khan <sabir.islam14@gmail.com>
2024-06-14 02:13:29 +0600

    Initial Commit

---
+++ src/main/java/com/asterisklab/spring_bot_practice/SpringBotPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.asterisklab.spring_bot_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBotPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBotPracticeApplication.class, args);
+	}
+
+}
